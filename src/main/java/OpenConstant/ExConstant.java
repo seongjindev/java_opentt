69095db3a281821efec8d875f3404a1a339765ac
bae <baesj11@gmail.com>
2021-06-05 17:23:05 +0900

    Constant
    상수

---
+++ src/main/java/OpenConstant/ExConstant.java
@@ -0,0 +1,2 @@
+package OpenConstant;public class Constant {
+}
--- src/main/java/javaopen/Day0507.java
+++
@@ -1,7 +0,0 @@
-package javaopen;
-
-public class Day0507 {
-    public static void main(String[] args) {
-        System.out.println(1.6+1.2);
-    }
-}
